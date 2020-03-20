cb7c1f1c9ade573602a1def827973d9b959f9d6a
Gerardo Nardelli <gerardo.nardelli@altoros.com>
2020-03-20 16:14:46 -0300

    add linter

--- my-plugin/src/MyPlugin.ts
+++ my-plugin/src/MyPlugin.ts
@@ -1,35 +1,35 @@
-import { BurnerPluginContext, Plugin, Actions } from '@burner-wallet/types';
-import MyPage from './ui/MyPage';
-import MyElement from './ui/MyElement';
+import { BurnerPluginContext, Plugin, Actions } from '@burner-wallet/types'
+import MyPage from './ui/MyPage'
+import MyElement from './ui/MyElement'
 
 interface PluginActionContext {
-  actions: Actions;
+  actions: Actions
 }
 
 export default class MyPlugin implements Plugin {
-  private pluginContext?: BurnerPluginContext;
+  private pluginContext?: BurnerPluginContext
 
   initializePlugin(pluginContext: BurnerPluginContext) {
-    this.pluginContext = pluginContext;
+    this.pluginContext = pluginContext
 
-    pluginContext.addPage('/my-page', MyPage);
+    pluginContext.addPage('/my-page', MyPage)
     pluginContext.addButton('apps', 'My Plugin', '/my-page', {
-      description: 'Sample plugin page',
-    });
-    pluginContext.addElement('home-middle', MyElement);
+      description: 'Sample plugin page'
+    })
+    pluginContext.addElement('home-middle', MyElement)
 
-    onQRScanned: ((scan: string, ctx: PluginActionContext) => {
+    onQRScanned: (scan: string, ctx: PluginActionContext) => {
       if (scan === 'My Plugin') {
-        ctx.actions.navigateTo('/my-page');
-        return true;
+        ctx.actions.navigateTo('/my-page')
+        return true
       }
-    });
+    }
   }
 
   async getBlockNum() {
-    const assets = this.pluginContext!.getAssets();
-    const web3 = this.pluginContext!.getWeb3(assets[0].network);
-    const blockNum = web3.eth.getBlockNumber();
-    return blockNum;
+    const assets = this.pluginContext!.getAssets()
+    const web3 = this.pluginContext!.getWeb3(assets[0].network)
+    const blockNum = web3.eth.getBlockNumber()
+    return blockNum
   }
 }
--- my-plugin/src/ui/MyElement.tsx
+++ my-plugin/src/ui/MyElement.tsx
@@ -1,23 +1,21 @@
-import React, { useEffect, useState } from 'react';
-import { PluginElementContext } from '@burner-wallet/types';
-import MyPlugin from '../MyPlugin';
+import React, { useEffect, useState } from 'react'
+import { PluginElementContext } from '@burner-wallet/types'
+import MyPlugin from '../MyPlugin'
 
 const MyElement: React.FC<PluginElementContext> = ({ plugin }) => {
-  const [block, setBlock] = useState<string | null>(null);
-  const _plugin = plugin as MyPlugin;
+  const [block, setBlock] = useState<string | null>(null)
+  const _plugin = plugin as MyPlugin
 
   useEffect(() => {
     _plugin.getBlockNum().then((num: number) => setBlock(num.toString()))
-  }, []);
+  }, [])
 
   return (
     <div>
       <div>Injected plugin element</div>
-      {block && (
-        <div>Current block number: {block}</div>
-      )}
+      {block && <div>Current block number: {block}</div>}
     </div>
-  );
-};
+  )
+}
 
-export default MyElement;
+export default MyElement
