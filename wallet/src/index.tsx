e6b7f8f29a2a277da74db3ae47050bf43b78f08f
Alexander Kolotov <alexandr.kolotov@gmail.com>
2020-07-15 00:35:30 +0300

    WETC exchange UI

--- wallet/src/index.tsx
+++ wallet/src/index.tsx
@@ -2,21 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import BurnerCore from '@burner-wallet/core'
 import { InjectedSigner, LocalSigner } from '@burner-wallet/core/signers'
-import { InfuraGateway, InjectedGateway, XDaiGateway } from '@burner-wallet/core/gateways'
+import { InfuraGateway, InjectedGateway } from '@burner-wallet/core/gateways'
 import ModernUI from '@burner-wallet/modern-ui'
 import Exchange from '@burner-wallet/exchange'
 import MetamaskPlugin from '@burner-wallet/metamask-plugin'
 // Import resources from our plugin
-import { Stake, xStake, StakeBridge } from 'my-plugin'
+import { Etc, Wetc, TokenBridgeGateway, WETCBridge } from '@poanet/tokenbridge-bw-exchange'
 
 const core = new BurnerCore({
   signers: [new InjectedSigner(), new LocalSigner({ privateKey: process.env.REACT_APP_PK, saveKey: false })],
-  gateways: [new InjectedGateway(), new XDaiGateway(), new InfuraGateway(process.env.REACT_APP_INFURA_KEY)],
-  assets: [Stake, xStake]
+  gateways: [new InjectedGateway(), new TokenBridgeGateway(), new InfuraGateway(process.env.REACT_APP_INFURA_KEY)],
+  assets: [Etc, Wetc]
 })
 
 const exchange = new Exchange({
-  pairs: [new StakeBridge()]
+  pairs: [new WETCBridge()]
 })
 
 const BurnerWallet = () => <ModernUI title="Burner Wallet" core={core} plugins={[exchange, new MetamaskPlugin()]} />
